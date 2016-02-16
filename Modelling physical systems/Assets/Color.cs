616dc95eb1e9881373973fcd237c1c87f8923201
Alexander Giakalis <agiaka15@student.aau.dk>
2016-02-16 14:17:33 +0100

    Alex

---
+++ Modelling physical systems/Assets/Color.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Color : MonoBehaviour {
+
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnTriggerEnter(Collider ball) {
+		
+	}
+
+}
+	
