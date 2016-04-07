80e9a72e43c08496e3dc62d691d391fd1240df16
Bianca Christensen <biclavio@gmail.com>
2016-04-07 17:31:49 +0200

    Play with speech boxes

---
+++ Game/Assets/Scripts/Button.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+    public Color defaultColour;
+    public Color selectedColour;
+
+    private Material mat;
+
+    void Start() {
+        mat = GetComponent<Renderer>().material;
+    }
+    void OnTouchDown() {
+        mat.color = selectedColour;
+    }
+    void OnTouchUp() {
+        mat.color = defaultColour;
+    }
+    void OnTouchStay() {
+        mat.color = selectedColour;
+    }
+    void OnTouchExit() {
+        mat.color = defaultColour;
+    }
+
+}
---
+++ Game/Assets/Scripts/TouchInput.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TouchInput : MonoBehaviour {
+    Camera cam;
+    public LayerMask touchInputMask;
+
+    private List<GameObject> touchList = new List<GameObject>();
+    private GameObject[] touchesOld;
+    private RaycastHit hit;
+
+    void Start() {
+        cam = GetComponent<Camera>();
+        touchList = new List<GameObject>();
+    }
+
+    void Update() {
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0)) {
+
+            touchesOld = new GameObject[touchList.Count];
+            touchList.CopyTo(touchesOld);
+            touchList.Clear();
+
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, touchInputMask)) {
+
+                GameObject recipient = hit.transform.gameObject;
+                touchList.Add(recipient);
+
+                if (Input.GetMouseButtonDown(0)) {
+                    recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+                if (Input.GetMouseButtonUp(0)) {
+                    recipient.SendMessage("OnTouchUp", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+                if (Input.GetMouseButton(0)) {
+                    recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+
+            foreach (GameObject g in touchesOld) {
+                if (!touchList.Contains(g)) {
+                    g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+#endif
+
+        if (Input.touchCount > 0) {
+            touchesOld = new GameObject[touchList.Count];
+            touchList.CopyTo(touchesOld);
+            touchList.Clear();
+
+            foreach (Touch touch in Input.touches) {
+                Ray ray = cam.ScreenPointToRay(touch.position);
+
+                /*Touch[] touches = Input.touches;
+                if (Input.touchCount >= 2) {
+                    Vector2 center = (touches[Input.touchCount - 1].position + touches[Input.touchCount].position)/2;
+                    Ray rayCenter = Camera.main.ScreenPointToRay(center);
+                    //if (Physics.Raycast(rayCenter, out hit))
+                    //  hit.rigidbody.rotation = new Quaternion(0, 0, 0, 0);
+                    //}*/
+                if (Physics.Raycast(ray, out hit, touchInputMask)) { //rayCenter
+                    GameObject recipient = hit.transform.gameObject;
+                    touchList.Add(recipient);
+
+                    if (touch.phase == TouchPhase.Began) {
+                        recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                    if (touch.phase == TouchPhase.Ended) {
+                        recipient.SendMessage("OnTouchUp", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                    if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) {
+                        recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                    if (touch.phase == TouchPhase.Canceled) {
+                        recipient.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                }
+            }
+            foreach (GameObject g in touchesOld) {
+                if (!touchList.Contains(g)) {
+                    g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+            //}
+        }
+    }
+}
