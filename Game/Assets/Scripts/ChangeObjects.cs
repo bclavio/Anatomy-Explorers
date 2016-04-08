cabe9213b70d075c97a0b9a217a26655f621ee2c
Bianca Christensen <biclavio@gmail.com>
2016-04-08 14:59:20 +0200

    Still working on dialogue boxes. Commit to change branch.

---
+++ Game/Assets/Scripts/ChangeObjects.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ChangeObjects : MonoBehaviour {
+
+    public GameObject speechObject;
+    public static Text uiSpeech;
+    public bool option1text = false;
+    //public static GameObject option1;
+
+    void Start () {
+        uiSpeech = speechObject.GetComponent<Text>();
+        //option1 = GameObject.Find("ButtonOption (1)");
+    }
+	
+	void Update () {
+        ChangeText();
+
+
+    }
+
+    public void ChangeText() {
+        // if option/character is true (+ variable), change test 
+        if (option1text) {
+            uiSpeech.text = "Text changed into something awesome!";            
+        }
+    }
+}
