8f180d03119255860e2d050a8d07239a38096278
Mark Kronborg Poulsen <kronborg-poulsen@hotmail.com>
2016-04-14 11:11:09 +0200

    HealthBar

---
+++ Game/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+    
+    public void ChangeHealthUp()
+    {
+        Image HealthBar = GetComponent<Image>();
+        HealthBar.fillAmount += .1f;
+    }
+    public void ChangehealthDown()
+    {
+        Image HealthBar = GetComponent<Image>();
+        HealthBar.fillAmount -= .1f;
+    }
+}
