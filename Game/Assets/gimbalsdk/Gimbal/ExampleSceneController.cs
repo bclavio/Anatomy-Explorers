0c6eb520ad7220252475e7ed6176a62cce6324e4
Alexandros Panagiotis Giakalis <agiaka15@student.aau.dk>
2016-04-17 11:56:57 +0200

    Gimbal Plugin

---
+++ Game/Assets/gimbalsdk/Gimbal/ExampleSceneController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ExampleSceneController : MonoBehaviour {
+	public GimbalBehavior gimbalBehavior;
+	public Text beaconStatusText;
+	public Text placeStatusText;
+	public Text beaconsFoundText;
+	public Text placesVisitingText;
+	public Text placesVisitedText;
+	public Button togglePlacesManagerButton;
+	public Button toggleBeaconManagerButton;
+
+	private Dictionary<string, BeaconSighting> sightings = new Dictionary<string, BeaconSighting>();
+	private Dictionary<string, Visit> currentVisits = new Dictionary<string, Visit>();
+	private Dictionary<string, Visit> endedVisits = new Dictionary<string, Visit>();
+
+	void Start () {
+		gimbalBehavior.BeaconSighted += new GimbalBehavior.BeaconSightingHandler(BeaconSightingFound);
+		gimbalBehavior.BeginVisit += new GimbalBehavior.BeginVisitHandler(StartedPlaceVisit);
+		gimbalBehavior.EndVisit += new GimbalBehavior.EndVisitHandler(EndedPlaceVisit);
+
+		placeStatusText.text = "Place Sighting: " + updateStatusText(gimbalBehavior.autoStartPlaceManager);
+		beaconStatusText.text = "Beacon Sighting: " + updateStatusText(gimbalBehavior.autoStartBeaconManager);
+	}
+
+	void Update () {
+		beaconsFoundText.text = "Beacons:";
+		foreach (BeaconSighting sighting in sightings.Values) {
+			beaconsFoundText.text += "\nName: " + sighting.beacon.name + "\t Identifier: " + sighting.beacon.identifier + "\t RSSI: " + sighting.rssi + "\t Date: " + sighting.date;
+		}
+
+		placesVisitingText.text = "Visiting:";
+		updateVisitTextHelper(placesVisitingText, currentVisits);
+
+		placesVisitedText.text = "Visited:";
+		updateVisitTextHelper(placesVisitedText, endedVisits);
+
+	}
+
+	private void updateVisitTextHelper(Text visitText, Dictionary<string, Visit> visits) {
+		foreach (Visit visit in visits.Values) {
+			visitText.text += "\nPlace Name: " + visit.place.name + "\t Identifier: " + visit.place.identifier + 
+				"\t Arrival Date: " + visit.arrivalDate + "\t Departure Date: " + visit.departureDate;
+		}
+	}
+	
+	private void BeaconSightingFound(BeaconSighting sighting) {
+		if (!sightings.ContainsKey(sighting.beacon.identifier)) {
+			sightings.Add(sighting.beacon.identifier, sighting);
+		} else {
+			sightings[sighting.beacon.identifier] = sighting;
+		}
+	}
+
+	private void StartedPlaceVisit(Visit visit) {
+		VisitHelper(visit, currentVisits, endedVisits);
+	}
+
+	private void EndedPlaceVisit(Visit visit) {
+		VisitHelper(visit, endedVisits, currentVisits);
+	}
+
+	private void VisitHelper(Visit visit, Dictionary<string, Visit> addVisit, Dictionary<string, Visit> removeVisit) {
+		if (!addVisit.ContainsKey(visit.place.identifier)) {
+			addVisit.Add(visit.place.identifier, visit);
+		} else {
+			addVisit[visit.place.identifier] = visit;
+		}
+
+		if (removeVisit.ContainsKey(visit.place.identifier)) {
+			removeVisit.Remove(visit.place.identifier);
+		}
+	}
+	
+	public void TogglePlacesManager() {
+		gimbalBehavior.TogglePlaceListening();
+		placeStatusText.text = "Place Sighting: " + updateStatusText(gimbalBehavior.IsListeningForPlaces);
+	}
+
+	public void ToggleBeaconManager() {
+		gimbalBehavior.ToggleBeaconListening();
+		beaconStatusText.text = "Beacon Sighting: " + updateStatusText(gimbalBehavior.IsListeningForBeacons);
+	}
+
+	private string updateStatusText(bool sightingStatus) {
+		if (sightingStatus) {
+			return "On";
+		} else {
+			return "Off";
+		}
+	}
+}
---
+++ Game/Assets/gimbalsdk/Gimbal/GimbalBehavior.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class GimbalBehavior : MonoBehaviour {
+	public string iosApiKey;
+	public string androidApiKey;
+	public bool autoStartBeaconManager = true;
+	public bool autoStartPlaceManager = true;
+
+	public delegate void BeaconSightingHandler(BeaconSighting sighting);
+	public event BeaconSightingHandler BeaconSighted = delegate {};
+
+	public delegate void BeginVisitHandler(Visit visit);
+	public event BeginVisitHandler BeginVisit = delegate {};
+
+	public delegate void EndVisitHandler(Visit visit);
+	public event EndVisitHandler EndVisit = delegate {};
+
+	private bool isListeningForBeacons;
+	private bool isListeningForPlaces;
+
+	public bool IsListeningForBeacons {
+		get { return isListeningForBeacons; }
+	}
+
+	public bool IsListeningForPlaces {
+		get { return isListeningForPlaces; }
+	}
+
+	void Start() {
+		if (gameObject.name != "GimbalPlugin") {
+			Debug.LogWarning("Gimbal callbacks will not be called unless the GimbalBehavior is attached to a game object named GimbalPlugin.");
+		}
+
+		SetApiKey();
+
+		if (autoStartBeaconManager) {
+			StartBeaconManager();
+		}
+
+		if (autoStartPlaceManager) {
+			StartPlaceManager();
+		}
+	}
+
+	public void ToggleBeaconListening() {
+		if (isListeningForBeacons) {
+			StopBeaconManager();
+		} else {
+			StartBeaconManager();
+		}
+	}
+
+	public void StartBeaconManager() {
+		Gimbal.StartBeaconManager();
+		isListeningForBeacons = true;
+	}
+	
+	public void StopBeaconManager() {
+		Gimbal.StopBeaconManager();
+		isListeningForBeacons = false;
+	}
+
+	public void TogglePlaceListening() {
+		if (isListeningForPlaces) {
+			StartPlaceManager();
+		} else {
+			StopPlaceManager();
+		}
+	}
+
+	public void StartPlaceManager() {
+		Gimbal.StartPlaceManager();
+		isListeningForPlaces = false;
+	}
+	
+	public void StopPlaceManager() {
+		Gimbal.StopPlaceManager();
+		isListeningForPlaces = true;
+	}
+	
+	public bool IsMonitoring() {
+		return Gimbal.IsMoitoring();
+	}
+
+	void SetApiKey() {
+#if UNITY_IPHONE
+		Gimbal.SetApiKey(iosApiKey);
+#elif UNITY_ANDROID
+		Gimbal.SetApiKey(androidApiKey);
+#endif
+	}
+	
+	void OnBeaconSighting(string message) {
+		JSONNode node = JSON.Parse(message);
+		BeaconSighting beaconSighting = new BeaconSighting(node);
+		BeaconSighted(beaconSighting);
+	}
+
+	void OnBeginVisit(string message) {
+		JSONNode node = JSON.Parse(message);
+		Visit visit = new Visit(node);
+		BeginVisit(visit);
+	}
+
+	void OnEndVisit(string message) {
+		JSONNode node = JSON.Parse(message);
+		Visit visit = new Visit(node);
+		EndVisit(visit);
+	}
+}
