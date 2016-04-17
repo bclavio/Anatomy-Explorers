4a7737de7ab26591b965c5cf29d9d4ab720e31eb
Alexandros Panagiotis Giakalis <agiaka15@student.aau.dk>
2016-04-17 11:34:38 +0200

    MapIndicator

---
+++ Game/Assets/Scripts/MapIndicator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MapIndicator : MonoBehaviour {
+
+	public void Change()
+    {
+        SceneManager.LoadScene("Map2");
+    }
+
+    public void Return()
+    {
+        SceneManager.LoadScene("MapScene");
+    }
+}
